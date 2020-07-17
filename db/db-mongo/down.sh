3181a4973291dfac3d760a70b839d2ef779f865a
Eli Harper <eli.harper@caci.com>
2020-07-17 13:57:33 -0400

    MongoDB in k8s with local persistent volumes is functional. Kubernetes DNS is being worked on by SA team at the moment, but the DB is unreachable from within k8s for this and for other reasons regarding STIGs and CACI policies restricting the k8s VM. The DB can, however, be reached by the application running on the jumpoff server.

--- db/db-mongo/down.sh
+++ db/db-mongo/down.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 kubectl delete pod mongo \
- && kubectl delete persistentvolumeclaim mongo-data-db \
- && kubectl delete persistentvolume mongo-data-db \
- && kubectl delete storageclass local \
- && kubectl delete svc db
+ ; kubectl patch po mongo-0 -p '{"metadata":{"finalizers":null}}' ; kubectl delete pod mongo-0 \
+ ; kubectl patch pvc data-db-mongo-0 -p '{"metadata":{"finalizers":null}}' ; kubectl delete persistentvolumeclaim data-db-mongo-0 \
+ ; kubectl delete persistentvolume mongo-data-db \
+ ; kubectl delete statefulsets mongo \
+ ; kubectl delete storageclass local \
+ ; kubectl delete svc mongo-service
--- db/db-mongo/up.sh
+++ db/db-mongo/up.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 kubectl apply -f localstorageclass.yaml \
- && kubectl apply -f ../db-service.yaml \
  && kubectl apply -f mongo-data-db-pv.yaml \
- && kubectl apply -f mongo-data-db-pvc.yaml \
  && kubectl apply -f mongo-ss.yaml \
+ && kubectl apply -f ../db-service.yaml \
+ && kubectl apply -f mongo-pod.yaml \
+ && kubectl apply -f mongo-data-db-pvc.yaml \
  && kubectl get po -w
